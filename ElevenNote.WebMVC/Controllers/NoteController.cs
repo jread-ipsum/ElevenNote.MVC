5019b6ecff5cf96f56d68e66805d263794e62510
Jordan Read <jread6477@gmail.com>
2021-10-14 18:24:22 -0400

    NoteEdit model, service, and controller added.

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -36,7 +36,7 @@ namespace ElevenNote.WebMVC.Controllers
             if (!ModelState.IsValid)
                 return View(model);
 
-             var service = CreateNoteService();
+            var service = CreateNoteService();
 
             if (service.CreateNote(model))
             {
@@ -53,6 +53,20 @@ namespace ElevenNote.WebMVC.Controllers
             var svc = CreateNoteService();
             var model = svc.GetNoteById(id);
 
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content
+                };
             return View(model);
         }
 
